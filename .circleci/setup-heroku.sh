5691ac282735922010905757f53098821a25aa70
Jason Carter <jason@mavenlink.com>
2018-02-09 18:22:19 -0700

    Try to add heroku remote.

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,3 +1,4 @@
+git remote add heroku https://git.heroku.com/urug-bot.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
@@ -7,6 +8,10 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
