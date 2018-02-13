795549f016eab0f21edcd7456e40807ff14b906f
Ben Eggett <beneggett@gmail.com>
2018-02-13 01:52:44 -0700

    Adds some more pretty stuff to the reddit bot

--- urug_bot/bot.rb
+++ urug_bot/bot.rb
@@ -9,7 +9,7 @@ module UrugBot
       end
 
       command 'reddit' do
-        desc 'Returns back 3 sorted posts for a given subreddit. `@reddit-bot reddit <subreddit> <top|new|controversial|rising|hot>`'
+        desc 'Returns back 3 sorted posts for a given subreddit. `@urug-bot reddit <subreddit> <top|new|controversial|rising|hot>`'
       end
     end
   end
